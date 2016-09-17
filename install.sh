91fc5f8a930e77fce8552a480e6773910995f4bf
Thomas Stringer <tr.stringer@outlook.com>
2016-09-17 19:34:10 -0400

    add sample config file copy

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/pymail.py"
+CONFIGPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.pymail.sample.json"
 echo "creating symlink to $SCRIPTPATH"
 ln -s $SCRIPTPATH /usr/local/bin/pymail
 
 echo "creating necessary folder structure"
 mkdir -p ~/pymail/archive
 mkdir -p ~/pymail/outbox/sent
+
+cp $CONFIGPATH ~/.pymail
